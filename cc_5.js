05ab0811f402eb75370252f79037c1e9560577a2
Alexis Pridemore <alpridemore@usf.edu>
2025-02-08 10:49:24 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,12 @@
+// Task 1: Object Properties, Scenario: Customer Profile
+
+let customer = {
+    name: "Micheal Jordan",
+    age: 55,
+    email: "mjordan@gmail.com"
+};
+
+console.log('name: $${customerName}');
+console.log('age: $${customerAge}');
+console.log('email: $${customerEmail}');
+
