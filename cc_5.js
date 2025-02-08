c563148bbff263f1d22bda2c9d0e25602f5cb9e0
Alexis Pridemore <alpridemore@usf.edu>
2025-02-08 11:52:52 -0500

    Task 4 - Price Adjustments

--- cc_5.js
+++ cc_5.js
@@ -28,10 +28,19 @@ order.displayOrder();       //Call the method to Log the order details
 
 // Task 3 Array Manipulation (push, pop, shift, unshift), Scenario: Shopping Cart
 
-let cartItems =["laptop", "airpods", "iphone"];
+let cartItems = ["laptop", "airpods", "iphone"];
 cartItems.push("ipad");                            //Add new product
 cartItems.pop();                                  //Remove last item
 cartItems.unshift("charger");                    //Add item at beginning
 cartItems.shift();                              //Remove first item
 
 console.log(cartItems);          //Log final array
+
+// Task 4 Map Method, Scenario: Price Adjustments
+
+let prices = [100, 200, 300];
+let discountedPrices = prices.map(price => price * 0.9);
+
+console.log(discountedPrices);
+
+
