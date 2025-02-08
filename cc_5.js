ae596fdf9a421b28a5ada951b5b750717c91b994
Alexis Pridemore <alpridemore@usf.edu>
2025-02-08 11:01:42 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -6,7 +6,22 @@ let customer = {
     email: "mjordan@gmail.com"
 };
 
-console.log('name: $${customerName}');
-console.log('age: $${customerAge}');
-console.log('email: $${customerEmail}');
+console.log('name: ${customerName}');
+console.log('age: ${customerAge}');
+console.log('email: ${customerEmail}');
 
+// Task 2: Object Methods, Scenario: Order Details
+
+let order = {
+    orderId: 456,
+    totalAmount: 1500,
+    status: "Processing",
+    
+    displayOrder: function() {
+        console.log('Order ID: ${this.orderId}');
+        console.log('Total Amount: $${this.totalAmount}');
+        console.log('Status: ${this.status}');
+    } 
+};
+
+order.displayOrder();       //Call the method to Log the order details
