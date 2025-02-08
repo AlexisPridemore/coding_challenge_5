e6035cdfafe50d00cda0c5d1ead0ba349ebfb63d
Alexis Pridemore <alpridemore@usf.edu>
2025-02-08 11:58:02 -0500

    Changed a number from the inventory, line 48

--- cc_5.js
+++ cc_5.js
@@ -45,7 +45,7 @@ console.log(discountedPrices);    //Log discounted prices
 
 // Task 5 Filter Method, Scenario: Product Availability
 
-let inventory = [50, 20, 60, 90, 100];
+let inventory = [50, 20, 0, 90, 100];
 let availableInventory = inventory.filter(quantity => quantity > 0);   //Use .filter() to remove products with zero stock
 
 console.log(availableInventory);    //Log filtered array
